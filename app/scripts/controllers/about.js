1176b3e14e16b40d0bc0147c029fe52ec57aebc6
quentinchap <chapelle.quentin@gmail.com>
2016-03-17 20:37:09 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sitePersoAng15App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sitePersoAng15App
+ */
+angular.module('sitePersoAng15App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sitePersoAng15App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sitePersoAng15App
+ */
+angular.module('sitePersoAng15App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
